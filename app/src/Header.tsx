387f5e9c67a4471bc0f29a35f0b9f019a0d3ac4b
Drew White <54377401+drewthemain@users.noreply.github.com>
2023-05-30 23:09:59 -0400

    Initial header and colors

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,9 +1,96 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import styled from "@emotion/styled";
+import { useNavigate, useLocation } from "react-router-dom";
+import { colors } from "./Style/colors";
+
+// Describes the app header
+const MainHeader = styled(AppBar)`
+    background: ${colors.black};
+`;
+
+// Container for the full header bar
+const HeaderContainer = styled(Toolbar)`
+    text-align: left;
+    display: flex;
+    justify-content: space-between;
+    margin: 2% 5%;
+`;
+
+// Describes each individual header button
+const HeaderButton = styled(Button)`
+    font-weight: bold;
+    font-size: 100%;
+    margin-left: 2vw;
+    color: ${colors.red};
+`;
+
+// Describes the text for the header message
+const HeaderText = styled(Typography)`
+  color: ${colors.white};
+  font-size: 200%;
+  font-weight: bold;
+`;
+
+// The various header button options with it's corresponding path
+const headerOptions = [
+    {
+      label: "Projects",
+      location: "projects",
+
+    },
+    {
+      label: "About Me",
+      location: "aboutme",
+    },
+    {
+      label: "Linkedin",
+      location: "",
+    }
+  ];
+
 /**
  * Acts as the static header at the top of the site
- * @returns the header bar
+ * @returns the header bar properly displayed
  */
 export default function Header() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    // Maps buttons to the proper values in headerOptions
+    const getMenuButtons = () => {
+        return headerOptions.map(({ label, location }) => {
+          return (
+            <HeaderButton
+              {...{
+                key: label,
+                onClick: () => navigate(`/${location}`)
+              }
+            }
+            >
+              {label}
+            </HeaderButton>
+          );
+        });
+      };
+    
+    // Returns the proper container for each header value
+    const displayWelcome = () => {
+        return <HeaderContainer>
+            {welcomeMessage}
+            <div>{getMenuButtons()}</div>
+            </HeaderContainer>
+    };
+
+    const welcomeMessage = (
+        <HeaderText variant="caption">
+          Drew White - Computer Science
+        </HeaderText>
+    );
+
+
     return (
-        <div></div>
+        <header>
+            <MainHeader>{displayWelcome()}</MainHeader>
+        </header>
     );
 }
---
+++ app/src/Style/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    onyx: "#403F4C",
+    raisin: "#2C2B3C",
+    metal: "#1B2432",
+    black: "#121420",
+    red: "#B76D68",
+    white: "#F9EBE0"
+}
